abee056d0c992467acbec0726a42f70cfbe1de44
matrikz <matrikz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-27 07:02:01 +0000

    * initial import of very basic extensions (only name and description)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@11474 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ trunk/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "caretaker_instance"
+#
+# Auto generated 27-08-2008 08:59
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Caretaker Instance',
+	'description' => 'Client for caretaker observation system',
+	'category' => 'misc',
+	'author' => 'Martin Ficzel,Thomas Hempel,Christopher Hlubek,Tobias Liebig',
+	'author_email' => 'ficzel@work.de,hempel@work.de,hlubek@networkteam.com,liebig@networkteam.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"9c48";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"9f12";s:20:"doc/wizard_form.html";s:4:"9378";}',
+);
+
+?>
