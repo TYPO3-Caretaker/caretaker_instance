2266382860bdca8942ef617af0912373de9ea248
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-05 15:58:47 +0000

    * Added seperate extension-skelettons for selenium- and snmp-tests. * Simplified the TestServiceInterface. Now only the method runTest has to be provided. * Refactored the db to assign tests and instances to groups. no more tests per instance. This will make the implementation of the testrunner much easier. * Removed dependency to tt_address and caretaker instance. * Moved Basic test Services to caretaker Instance Extension. In future the caretaker instance on the server will talk to the caretaker instance on the host.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@17515 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ trunk/ext_tables.php
@@ -0,0 +1,14 @@
+<?php 
+
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+	// load Service Helper
+include_once(t3lib_extMgm::extPath('caretaker').'classes/class.tx_caretaker_ServiceHelper.php');
+
+	// register Tests
+tx_caretaker_ServiceHelper::registerCaretakerService ($_EXTKEY , 'services' , 'tx_caretaker_typo3_info'   ,'TYPO3-> Info', 'Retrieves the version of TYPO3' );
+tx_caretaker_ServiceHelper::registerCaretakerService ($_EXTKEY , 'services' , 'tx_caretaker_typo3_extensions',  'TYPO3 -> Extensions' , 'Retrieves a list of all available extensions (includes paths, versions and status)' );
+tx_caretaker_ServiceHelper::registerCaretakerService ($_EXTKEY , 'services' , 'tx_caretaker_system_info',  'System -> Info' , 'Retrieves System Informations' );
+tx_caretaker_ServiceHelper::registerCaretakerService ($_EXTKEY , 'services' , 'tx_caretaker_system_load',  'System -> Load' , 'Retrieves System Status Infos' );
+
+?>
