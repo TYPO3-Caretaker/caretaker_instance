bbea6c63fdf7ae141ce85a969756202970c3fb48
networkteam_hlubek <networkteam_hlubek@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-16 16:07:27 +0000

    Added base remote test service, implemented some logic of the extension test service
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@20428 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/services/class.tx_caretakerinstance_ExtensionTestService.php
+++ trunk/services/class.tx_caretakerinstance_ExtensionTestService.php
@@ -33,29 +33,97 @@
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-require_once (t3lib_extMgm::extPath('caretaker').'/services/class.tx_caretaker_TestServiceBase.php');
+require_once(t3lib_extMgm::extPath('caretaker_instance', 'services/class.tx_caretakerinstance_RemoteTestServiceBase.php'));
 
-class tx_caretakerinstance_ExtensionTestService extends tx_caretaker_TestServiceBase {
+class tx_caretakerinstance_ExtensionTestService extends tx_caretakerinstance_RemoteTestServiceBase{
 	
-	public function runTest(){
+	public function runTest() {
 		
-		$extension_key = $this->getConfigValue('extension_key');
+		$extensionKey = $this->getConfigValue('extension_key');
+		$requirementMode = $this->getConfigValue('requirement_mode');
 		
-		if (!$extension_key){
-			return  tx_caretaker_TestResult::create( TX_CARETAKER_STATE_UNDEFINED, 0 , 'No extension key was given' );
+		if (!$extensionKey){
+			return tx_caretaker_TestResult::create(TX_CARETAKER_STATE_UNDEFINED, 0, 'Cannot execute extension test without extension key');
 		}
 		
-		// here happens magic
-		 
-		$testResult =  tx_caretaker_TestResult::create( TX_CARETAKER_STATE_OK, 0 , 'Port was not defined' );
+		$operation = array('GetExtensionVersion', array('extensionKey' => $extensionKey));
+
+		$instanceUrl = $this->instance->getUrl();
+		$instancePublicKey = $this->instance->getPublicKey();
+
+		// TODO execute operation on instance
+		// $commandResult = ...
 		
+		if (!$commandResult->isSuccessful()) {
+			return tx_caretaker_TestResult::create(TX_CARETAKER_STATE_UNDEFINED, 0, 'Command execution failed: ' . $commandResult->getMessage());
+		}
+		
+		$results = $commandResult->getOperationResults();
+		$operationResult = $results[0];
+		// Status can be true or false
+		$status = $operationResult->getStatus();
+		// The value is the extension version if status is true
+		if ($status === TRUE) {
+			$extensionVersion = $operationResult->getValue();
+		} else {
+			$extensionVersion = '';
+		}
+		
+		$checkResult = $this->checkVersionForRequirementAndVersionRange(
+			$extensionVersion,
+			$requirementMode,
+			$minVersion,
+			$maxVersion);
+		if ($checkResult === TRUE) {
+			$testResult = tx_caretaker_TestResult::create(TX_CARETAKER_STATE_OK, 0);
+		} else {
+			$testResult = tx_caretaker_TestResult::create(TX_CARETAKER_STATE_ERROR, 0, 'Extension check for [' . $extensionKey . '] failed: ' . $checkResult);
+		}
+
 		return $testResult;
-		
 	}
 	
+	public function checkVersionForRequirementAndVersionRange($actualValue, $requirement, $minVersion, $maxVersion) {
+		if ($requirement == 'none') {
+			if ($actualValue) {
+				return $this->checkVersionRange($actualValue, $minVersion, $maxVersion);
+			} else {
+				return TRUE;
+			}
+		}
+	}
+	
+	protected function checkVersionRange($actualVersion, $minVersion, $maxVersion) {
+		if ($minVersion == '') {
+			$minVersion = '0.0.0';
+		}
+		if ($maxVersion == '') {
+			$maxVersion = '9999.9999.9999';
+		}
+		list($actualMajor, $actualMinor, $actualRelease) = explode('.', $actualVersion);
+		list($minMajor, $minMinor, $minRelease) = explode('.', $minVersion);
+		list($maxMajor, $maxMinor, $maxRelease) = explode('.', $maxVersion);
+		/*
+		$actualMajor = intval($actualMajor);
+		$actualMinor = intval($actualMinor);
+		$actualRelease = intval($actualRelease);
+		$minMajor = intval($minMajor);
+		$minMinor = intval($minMinor);
+		$minRelease = intval($minRelease);
+		$maxMajor = intval($maxMajor);
+		$maxMinor = intval($maxMinor);
+		$maxRelease = intval($maxRelease);
+		*/
+		
+		$b1 = $actualMajor >= $minMajor && $actualMajor <= $maxMajor;
+		$b2 = $actualMinor >= $minMinor && $actualMinor <= $maxMinor;
+		$b3 = $actualRelease >= $minRelease && $actualRelease <= $maxRelease;
+		var_dump($b1, $b2, $b3);
+		return $b1 && $b2 && $b3;
+	}	
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/caretaker/services/class.tx_caretaker_ExtensionTestService.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/caretaker/services/class.tx_caretaker_ExtensionTestService.php']);
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/caretaker_instance/services/class.tx_caretaker_ExtensionTestService.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/caretaker_instance/services/class.tx_caretaker_ExtensionTestService.php']);
 }
 ?>
--- trunk/tests/class.tx_caretakerinstance_ExtensionTestService_testcase.php
+++ trunk/tests/class.tx_caretakerinstance_ExtensionTestService_testcase.php
@@ -1,5 +1,49 @@
 <?php
 class tx_caretakerinstance_ExtensionTestService_testcase extends tx_phpunit_testcase {
-	
+	public function testExtensionMustNotBeInstalledForRequirementNone() {
+		$service = new tx_caretakerinstance_ExtensionTestService();
+		$result = $service->checkVersionForRequirementAndVersionRange(
+			'', // Actual version
+			'none', // Requirement mode
+			'1.2', // Minimal allowed version
+			'' // Maximal allowed version
+		);
+		$this->assertTrue($result);
+	}
+
+	public function testExtensionVersionHasToBeInVersionRangeIfVersionGiven() {
+		$service = new tx_caretakerinstance_ExtensionTestService();
+		$result = $service->checkVersionForRequirementAndVersionRange(
+			'1.3.1', // Actual version
+			'none', // Requirement mode
+			'1.2.0', // Minimal allowed version
+			'' // Maximal allowed version
+		);
+		$this->assertTrue($result);
+
+		$result = $service->checkVersionForRequirementAndVersionRange(
+			'1.3.1', // Actual version
+			'none', // Requirement mode
+			'1.2.0', // Minimal allowed version
+			'1.5.3' // Maximal allowed version
+		);
+		$this->assertTrue($result);
+
+		$result = $service->checkVersionForRequirementAndVersionRange(
+			'1.1.4', // Actual version
+			'none', // Requirement mode
+			'1.2.5', // Minimal allowed version
+			'' // Maximal allowed version
+		);
+		$this->assertTrue($result !== TRUE);
+
+		$result = $service->checkVersionForRequirementAndVersionRange(
+			'1.5.7', // Actual version
+			'none', // Requirement mode
+			'1.2.8', // Minimal allowed version
+			'1.4.18' // Maximal allowed version
+		);
+		$this->assertTrue($result !== TRUE);
+	}
 }
 ?>
