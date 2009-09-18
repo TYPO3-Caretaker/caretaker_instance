7065a7e40f8656fcfe31c691e992f2fb092d9071
etobi.de <etobi.de@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-18 08:30:46 +0000

    raise version number due to the last changes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@24618 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
