f77f9492395c4127ab372f579734ebf1c392a415
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-27 13:31:12 +0000

    Added service registration to ext_localconf.php AND ext_tables.php.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@25906 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_tables.php
+++ trunk/ext_tables.php
@@ -4,6 +4,13 @@ if (!defined ('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-
+// Register Caretaker Services
+if (t3lib_extMgm::isLoaded('caretaker') ){
+	include_once(t3lib_extMgm::extPath('caretaker') . 'classes/class.tx_caretaker_ServiceHelper.php');
+	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_Extension',  'TYPO3 -> Extension', 'Check for a specific Extension');
+	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_TYPO3Version',  'TYPO3 -> Version', 'Check for the TYPO3 version');
+	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_FindUnsecureExtension',  'TYPO3 -> Find Unsecure Extenions', 'Find Extensions wich are marked unsecure in TER');
+	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_BackendUser',  'TYPO3 -> Check backend user accounts', 'Find unwanted backend user accounts');
+}
 
 ?>
