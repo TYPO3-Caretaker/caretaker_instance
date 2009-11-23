f04a479811b2e948f6660ca6bec34a934b3f7437
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-23 18:45:57 +0000

    required update after moving the service-helper from classes to classes/helpers
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@26851 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_localconf.php
+++ trunk/ext_localconf.php
@@ -12,7 +12,7 @@ foreach (array('GetPHPVersion', 'GetTYPO3Version', 'GetExtensionVersion','GetExt
 
 // Register Caretaker Services
 if (t3lib_extMgm::isLoaded('caretaker') ){
-	include_once(t3lib_extMgm::extPath('caretaker') . 'classes/class.tx_caretaker_ServiceHelper.php');
+	include_once(t3lib_extMgm::extPath('caretaker') . 'classes/helpers/class.tx_caretaker_ServiceHelper.php');
 	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_Extension',  'TYPO3 -> Extension', 'Check for a specific Extension');
 	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_TYPO3Version',  'TYPO3 -> Version', 'Check for the TYPO3 version');
 	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_FindUnsecureExtension',  'TYPO3 -> Find Unsecure Extenions', 'Find Extensions wich are marked unsecure in TER');
