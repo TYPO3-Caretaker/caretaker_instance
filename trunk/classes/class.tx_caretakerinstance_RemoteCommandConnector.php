184e25583b4cfe68ff0de38d24ee63739c4b84c6
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-16 08:30:00 +0000

    Added curl timeouts for the instance http requests. 10s / 30s Connect / request. I hope this makes caretaker_instance tests a bit more stable.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@27764 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/classes/class.tx_caretakerinstance_RemoteCommandConnector.php
+++ trunk/classes/class.tx_caretakerinstance_RemoteCommandConnector.php
@@ -252,6 +252,8 @@ class tx_caretakerinstance_RemoteCommandConnector {
 		curl_setopt($curl, CURLOPT_URL, $requestUrl);
 		curl_setopt($curl, CURLOPT_HEADER, 0);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 30);
 
 		$headers = array(
             "Cache-Control: no-cache",
