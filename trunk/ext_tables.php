38d042b5bdb60ab83c76b52995f63890a03cf5c7
matrikz <matrikz@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-22 09:27:04 +0000

    * Extracted service registration from ext_localconf and ext_tables and put that into ext_conf_include.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@30297 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_tables.php
+++ trunk/ext_tables.php
@@ -4,14 +4,6 @@ if (!defined ('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-// Register Caretaker Services
-if (t3lib_extMgm::isLoaded('caretaker') ){
-	include_once(t3lib_extMgm::extPath('caretaker') . 'classes/helpers/class.tx_caretaker_ServiceHelper.php');
-	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_Extension',  'TYPO3 -> Extension', 'Check for a specific Extension');
-	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_TYPO3Version',  'TYPO3 -> Version', 'Check for the TYPO3 version');
-	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_FindInsecureExtension',  'TYPO3 -> Find insecure Extenions', 'Find Extensions wich are marked insecure in TER');
-	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_BackendUser',  'TYPO3 -> Check backend user accounts', 'Find unwanted backend user accounts');
-	tx_caretaker_ServiceHelper::registerCaretakerService($_EXTKEY, 'services', 'tx_caretakerinstance_FindBlacklistedBePassword',  'TYPO3 -> Check be-password blacklist', 'Find backend user accounts with blacklisted passwords.');
-}
+require(t3lib_extMgm::extPath('caretaker_instance').'/ext_conf_include.php');
 
 ?>
