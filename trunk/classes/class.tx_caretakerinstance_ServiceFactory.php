7cc3dc78933dfc8d82e55a170277587b0a2a40fd
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-17 07:42:50 +0000

    Fixed bug in tx_caretakerinstamce_ServiceFactory. A parameter was expected in getRemoteCommandConnector but was not used. Relates #9154
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@36865 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/classes/class.tx_caretakerinstance_ServiceFactory.php
+++ trunk/classes/class.tx_caretakerinstance_ServiceFactory.php
@@ -134,7 +134,7 @@ class tx_caretakerinstance_ServiceFactory {
 	/**
 	 * @return tx_caretakerinstance_RemoteCommandConnector
 	 */
-	public function getRemoteCommandConnector($options) {
+	public function getRemoteCommandConnector() {
 		if ($this->remoteCommandConnector == null) {
 			$this->remoteCommandConnector = new tx_caretakerinstance_RemoteCommandConnector(
 				$this->getCryptoManager(),
