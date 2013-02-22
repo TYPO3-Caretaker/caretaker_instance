fd9deb0f0d14e19a7acbaf1b71030007d5077c73
Christian Weiske <christian.weiske@netresearch.de>
2013-02-22 13:54:43 +0100

    fix ExtensionTestServiceTest

--- tests/class.tx_caretakerinstance_ExtensionTestServiceTest.php
+++ tests/class.tx_caretakerinstance_ExtensionTestServiceTest.php
@@ -23,6 +23,8 @@
  * This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
+require_once(t3lib_extMgm::extPath('caretaker_instance', 'services/class.tx_caretakerinstance_ExtensionTestService.php'));
+
 /**
  * This is a file of the caretaker project.
  * http://forge.typo3.org/projects/show/extension-caretaker
