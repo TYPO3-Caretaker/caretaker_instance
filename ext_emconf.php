af125f44ca66d4d1943ff12f940765e9188e6d7a
Jan Haffner <info@jan-haffner.de>
2020-01-28 20:25:52 +0100

    [TASK] Set version to 3.0.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 0,
   'lockType' => '',
   'author_company' => '',
-  'version' => '2.1.0',
+  'version' => '3.0.0-dev',
   'constraints' => 
   array (
     'depends' =>
