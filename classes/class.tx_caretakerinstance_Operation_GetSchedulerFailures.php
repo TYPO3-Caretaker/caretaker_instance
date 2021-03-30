8aec6a476d06094d67b682af58db702b81e63830
Oliver Eglseder <oliver.eglseder@in2code.de>
2021-03-30 20:37:21 +0200

    [FEATURE] Add test to check for scheduler failures

---
+++ classes/class.tx_caretakerinstance_Operation_GetSchedulerFailures.php
@@ -0,0 +1,67 @@
+<?php
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2021 by Oliver Eglseder <oliver.eglseder@in2code.de>
+ *
+ * All rights reserved
+ *
+ * This script is part of the Caretaker project. The Caretaker project
+ * is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * This operation select all recent failures from the Scheduler Task table.
+ * This feature can not be implemented using GetRecord since there is no TCA for the table tx_scheduler_task
+ *
+ * @author Oliver Eglseder <oliver.eglseder@in2code.de>
+ */
+class tx_caretakerinstance_Operation_GetSchedulerFailures implements tx_caretakerinstance_IOperation, SingletonInterface
+{
+    public function execute($parameter = array()): tx_caretakerinstance_OperationResult
+    {
+        $results = [];
+
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class);
+        $query = $connection->getQueryBuilderForTable('tx_scheduler_task');
+        $query->select('*')->from('tx_scheduler_task');
+        $statement = $query->execute();
+        foreach ($statement as $row) {
+            $taskUid = $row['uid'];
+            if (empty($row['lastexecution_failure'])) {
+                continue;
+            }
+            try {
+                $exception = unserialize($row['lastexecution_failure'], ['allowed_classes' => false]);
+            } catch (Throwable $exception) {
+                $results[$taskUid] = 'unserialize exception: ' . (string)$exception;
+                continue;
+            }
+            $keepKeys = ['code', 'file', 'line', 'message'];
+            foreach (array_keys($exception) as $key) {
+                if (!in_array($key, $keepKeys, true)) {
+                    unset($exception[$key]);
+                }
+            }
+            $results[$taskUid] = $exception;
+        }
+        return new tx_caretakerinstance_OperationResult(empty($results), $results);
+    }
+}
---
+++ services/class.tx_caretakerinstance_SchedulerFailuresTestService.php
@@ -0,0 +1,66 @@
+<?php
+
+class tx_caretakerinstance_SchedulerFailuresTestService extends tx_caretakerinstance_RemoteTestServiceBase
+{
+    /**
+     * {@inheritDoc}
+     * @return tx_caretaker_TestResult
+     */
+    public function runTest(): tx_caretaker_TestResult
+    {
+        $operations = [
+            ['GetScheduler', []]
+        ];
+
+        $commandResult = $this->executeRemoteOperations($operations);
+
+        if (!$this->isCommandResultSuccessful($commandResult)) {
+            return $this->getFailedCommandResultTestResult($commandResult);
+        }
+
+        $results = $commandResult->getOperationResults();
+
+        $errors = [];
+
+        /** @var tx_caretakerinstance_OperationResult $operationResult */
+        foreach ($results as $operationResult) {
+            if (!$operationResult->isSuccessful()) {
+                $exceptions = $operationResult->getValue();
+                if (is_string($exceptions)) {
+                    if ('Operation [GetScheduler] unknown' === $exceptions) {
+                        return tx_caretaker_TestResult::create(
+                            tx_caretaker_Constants::state_error,
+                            0,
+                            'The Instance does not support this Operation. Did you forget to install the additional extension?' . PHP_EOL . 'Original Exception: ' . $exceptions
+                        );
+                    }
+                    return tx_caretaker_TestResult::create(
+                        tx_caretaker_Constants::state_error,
+                        0,
+                        'Command execution failed: ' . $exceptions
+                    );
+                }
+                foreach ($exceptions as $taskUid => $exception) {
+                    $errors[] = sprintf(
+                        'Scheduler [%d] failed with message: Exception %d in %s line %d "%s"',
+                        $taskUid,
+                        $exception['code'],
+                        $exception['file'],
+                        $exception['line'],
+                        $exception['message']
+                    );
+                }
+            }
+        }
+
+        if (!empty($errors)) {
+            return tx_caretaker_TestResult::create(
+                tx_caretaker_Constants::state_error,
+                0,
+                'Operation execution failed: ' . PHP_EOL . implode(PHP_EOL, $errors)
+            );
+        }
+
+        return tx_caretaker_TestResult::create(tx_caretaker_Constants::state_ok, 0, 'No Scheduler errors found');
+    }
+}
